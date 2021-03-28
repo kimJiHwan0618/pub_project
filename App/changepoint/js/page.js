4a081f933b2a0837a5efb8496c8661cea4c1dee2
kimJiHwan0618 <wlghks0618@kakao.com>
2021-03-28 21:56:25 +0900

    이미지 경로 수정

--- App/changepoint/js/page.js
+++ App/changepoint/js/page.js
@@ -7,13 +7,13 @@ $(function(){
         var i = document.getElementById("page").value;
 
         if(i==1){
-            $(".one").attr("src","../img/home_on.svg")
+            $(".one").attr("src","img/home_on.svg")
         }else if(i==2){
-            $(".two").attr("src","../img/notification_on.svg")
+            $(".two").attr("src","img/notification_on.svg")
         }else if(i==3){
-            $(".three").attr("src","../img/my-information_on.svg")
+            $(".three").attr("src","img/my-information_on.svg")
         }else if(i==4){
-            $(".four").attr("src","../img/all-menu_on.svg")
+            $(".four").attr("src","img/all-menu_on.svg")
         }
     }
    });
