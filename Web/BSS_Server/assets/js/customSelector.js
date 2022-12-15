a64a36a18cd0c8d5187373c69f9f1b7199d064f6
kimJiHwan0618 <wlghks0618@kakao.com>
2022-12-15 23:14:08 +0900

    Bss Server Css 작업 -> 대시보드, Common Chart, Grid 추가

--- Web/BSS_Server/assets/js/customSelector.js
+++ Web/BSS_Server/assets/js/customSelector.js
@@ -1,8 +1,16 @@
 window.addEventListener("load", function () {
   const selectorEl = document.querySelectorAll(".selector__label .list");
+  const list = document.querySelectorAll(".list a");
+  console.log(list);
+  for (let i = 0; i < list.length; i++) {
+    list[i].onclick = (e) => {
+      e.preventDefault();
+    };
+  }
 
   const selectorClick = (idx) => {
     selectorEl[idx].onclick = () => {
+      console.log(selectorEl[idx].parentNode.parentNode.childNodes[1]);
       selectorEl[idx].parentNode.parentNode.childNodes[1].blur();
     };
   };
