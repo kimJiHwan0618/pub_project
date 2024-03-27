049755c44f1b8f492c55f3f1a3740fca81a7834d
kimJiHwan0618 <78426129+kimJiHwan0618@users.noreply.github.com>
2024-03-27 16:12:49 +0900

    드래그앤 드랍 이벤트 재연결

--- web/ubcure/assets/js/prescription.js
+++ web/ubcure/assets/js/prescription.js
@@ -5,71 +5,77 @@ $(function () {
   });
 
   // 12주 템플릿 만들기
-  for (let i = 0; i < 12; i++) {
-    $(".prescription__bottom").append(`
-      <div class="week__list__item">
-        <div class="title__wrap">
-          <h4>${i + 1}주차</h4>
-          <button class="custom__btn delete small">
-            <span>
-              <img src="../assets/images/icon/ic-remove.svg" alt="삭제 아이콘"></img>
-            </span>
-            <p>비우기</p>
-          </button>
+  const templateInit = () => {
+    for (let i = 0; i < 12; i++) {
+      $(".prescription__bottom").append(`
+        <div class="week__list__item">
+          <div class="title__wrap">
+            <h4>${i + 1}주차</h4>
+            <button class="custom__btn delete small">
+              <span>
+                <img src="../assets/images/icon/ic-remove.svg" alt="삭제 아이콘"></img>
+              </span>
+              <p>비우기</p>
+            </button>
+          </div>
+          <ul>
+            <li class="box__shadow__03" 
+                ondrop="drop(event, 'add')"
+                ondragover="allowDrop(event)"
+                id="drageItem${i}_01"
+            >
+              <p class="day__num">1</p>
+            </li>
+            <li class="box__shadow__03" 
+                ondrop="drop(event, 'add')"
+                ondragover="allowDrop(event)"
+                id="drageItem${i}_02"
+            >
+              <p class="day__num">2</p>
+            </li>
+            <li class="box__shadow__03" 
+                ondrop="drop(event, 'add')"
+                ondragover="allowDrop(event)"
+                id="drageItem${i}_03"
+            >
+              <p class="day__num">3</p>
+            </li>
+            <li class="box__shadow__03" 
+                ondrop="drop(event, 'add')"
+                ondragover="allowDrop(event)"
+                id="drageItem${i}_04"
+            >
+              <p class="day__num">4</p>
+            </li>
+            <li class="box__shadow__03" 
+                ondrop="drop(event, 'add')"
+                ondragover="allowDrop(event)"
+                id="drageItem${i}_05"
+            >
+              <p class="day__num">5</p>
+            </li>
+            <li class="box__shadow__03" 
+                ondrop="drop(event, 'add')"
+                ondragover="allowDrop(event)"
+                id="drageItem${i}_06"
+            >
+              <p class="day__num">6</p>
+            </li>
+            <li class="box__shadow__03" 
+                ondrop="drop(event, 'add')"
+                ondragover="allowDrop(event)"
+                id="drageItem${i}_07"
+            >
+              <p class="day__num">7</p>
+            </li>
+          </ul>
         </div>
-        <ul>
-          <li class="box__shadow__03" 
-              ondrop="drop(event, 'add')"
-              ondragover="allowDrop(event)"
-              id="drageItem${i}_01"
-          >
-            <p class="day__num">1</p>
-          </li>
-          <li class="box__shadow__03" 
-              ondrop="drop(event, 'add')"
-              ondragover="allowDrop(event)"
-              id="drageItem${i}_02"
-          >
-            <p class="day__num">2</p>
-          </li>
-          <li class="box__shadow__03" 
-              ondrop="drop(event, 'add')"
-              ondragover="allowDrop(event)"
-              id="drageItem${i}_03"
-          >
-            <p class="day__num">3</p>
-          </li>
-          <li class="box__shadow__03" 
-              ondrop="drop(event, 'add')"
-              ondragover="allowDrop(event)"
-              id="drageItem${i}_04"
-          >
-            <p class="day__num">4</p>
-          </li>
-          <li class="box__shadow__03" 
-              ondrop="drop(event, 'add')"
-              ondragover="allowDrop(event)"
-              id="drageItem${i}_05"
-          >
-            <p class="day__num">5</p>
-          </li>
-          <li class="box__shadow__03" 
-              ondrop="drop(event, 'add')"
-              ondragover="allowDrop(event)"
-              id="drageItem${i}_06"
-          >
-            <p class="day__num">6</p>
-          </li>
-          <li class="box__shadow__03" 
-              ondrop="drop(event, 'add')"
-              ondragover="allowDrop(event)"
-              id="drageItem${i}_07"
-          >
-            <p class="day__num">7</p>
-          </li>
-        </ul>
-      </div>
-    `);
+      `);
+    }
+    $(".custom__btn.delete").click(function () {
+      $(".week__list__item").remove()
+      templateInit()
+    })
   }
   // // 12주 템플릿 만들기
 
@@ -91,4 +97,10 @@ $(function () {
       $(".category__list li").removeClass("on");
     }
   });
+
+  templateInit()
+  $(".custom__btn.delete").click(function () {
+    $(".week__list__item").remove()
+    templateInit()
+  })
 });
