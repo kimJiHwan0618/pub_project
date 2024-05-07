e4467dfd815a5f721cc722efa57814d69395d116
kimJiHwan0618 <78426129+kimJiHwan0618@users.noreply.github.com>
2024-05-08 01:15:07 +0900

    mypopol ptid02 변수에 따른 스타일 적용

--- web/mypopol/ptid02/js/init.js
+++ web/mypopol/ptid02/js/init.js
@@ -11,6 +11,17 @@ $(function () {
       $('html').animate({ scrollTop: sectionArray[idx].offsetTop - 50 }, 10);
     }
   });
+  $('.mobile-nav li p').click(function () {
+    const idx = $(this).parent().index();
+    if (scrollY < idx) {
+      $('html').animate({ scrollTop: sectionArray[idx].offsetTop - 20 }, 10);
+    } else {
+      $('html').animate({ scrollTop: sectionArray[idx].offsetTop - 50 }, 10);
+    }
+    $('header').removeClass('on');
+    $('.mobileMenutBar').removeClass('on');
+    $('.modal-bg').removeClass('on');
+  });
 
   let lastScrollY = 0;
   $(document).scroll(function () {
@@ -37,12 +48,7 @@ $(function () {
       }
       for (let i = 0; i < projectArray.length; i++) {
         if (projectArray[i].offsetTop - 500 < $(document).scrollTop()) {
-          if (projectIdx < i) {
-            projectIdx = i;
-            $('.project-item').eq(i).addClass('on');
-          }
-        } else {
-          if (projectIdx > i) {
+          if (projectIdx <= i) {
             projectIdx = i;
             $('.project-item').eq(i).addClass('on');
           }
@@ -67,7 +73,7 @@ $(function () {
         }
       }
       for (let i = 0; i < projectArray.length; i++) {
-        if (projectArray[i].offsetTop + 50 < $(document).scrollTop()) {
+        if (projectArray[i].offsetTop + 30 < $(document).scrollTop()) {
           if (projectIdx < i) {
             projectIdx = i;
             $('.project-item')
@@ -81,7 +87,9 @@ $(function () {
             $('.project-item')
               .eq(i + 1)
               .removeClass('on');
-            $('.project-item').eq(i).addClass('on');
+            projectIdx !== 0
+              ? $('.project-item').eq(i).addClass('on')
+              : $('.project-item').eq(0).removeClass('on');
           }
         }
       }
@@ -91,5 +99,28 @@ $(function () {
   const activeSection = (idx) => {
     $('.gnb li').removeClass('active');
     $('.gnb li').eq(idx).addClass('active');
+    $('.mobile-nav li').removeClass('active');
+    $('.mobile-nav li').eq(idx).addClass('active');
   };
+  // mobile
+  $('#nav-icon1').click(function () {
+    $('header').toggleClass('on');
+    $('.mobileMenutBar').toggleClass('on');
+    $('.modal-bg').toggleClass('on');
+  });
+  $('.modal-bg').click(function () {
+    $('header').removeClass('on');
+    $('.mobileMenutBar').removeClass('on');
+    $('.modal-bg').removeClass('on');
+  });
+  /* 'rgb(255, 182, 59)','rgb(75, 135, 224)','rgb(75, 224, 149)','rgb(55,65,81)' */
+  const mainColor = 'rgb(55,65,81)';
+  const rgbaTxt = mainColor.replace('rgb', 'rgba').replace(')', '');
+  document.documentElement.style.setProperty('--main-color', mainColor);
+  document.documentElement.style.setProperty('--main-color_08', rgbaTxt + ', .8)');
+  document.documentElement.style.setProperty('--main-color_06', rgbaTxt + ', .6)');
+  document.documentElement.style.setProperty('--main-color_04', rgbaTxt + ', .4)');
+  document.documentElement.style.setProperty('--main-color_02', rgbaTxt + ', .2)');
+  document.documentElement.style.setProperty('--main-color_01', rgbaTxt + ', .1)');
+  document.documentElement.style.setProperty('--main-color_005', rgbaTxt + ', .05)');
 });
--- web/mypopol/ptid02/js/section03.js
+++ web/mypopol/ptid02/js/section03.js
@@ -1,23 +1,35 @@
 $(function () {
   const skills = {
     'Front-End': ['HTML', 'Css', 'Javascript'],
-    'Back-End': ['Spring Boot', 'MySQL'],
+    'Back-End': ['SpringBoot', 'MySQL'],
     Etc: ['Git', 'Svn', 'Docker'],
   };
-  const [key, value] = Object.entries(skills)[0];
-  $('.about-skill-box .big-img img').attr(
-    'src',
-    `https://site.mypopol.com/ptid02/src/img/skills/${value[0].toLowerCase()}.svg`
-  );
-  $('.about-skill-box .left .top .text dt').text(value[0]);
-  for (let skill of skills[key]) {
-    $('.about-skill-box .left .bottom').append(`
+  const categorySkillsInit = (key) => {
+    $('.about-skill-box .big-img img').attr(
+      'src',
+      `https://site.mypopol.com/ptid02/src/img/skills/${skills[key][0].toLowerCase()}.svg`
+    );
+    $('.about-skill-box .left .top .text dt').text(skills[key][0]);
+    for (let skill of skills[key]) {
+      $('.about-skill-box .left .bottom').append(`
       <span>
       ${skill}
         <img src="https://site.mypopol.com/ptid02/src/img/skills/${skill.toLowerCase()}.svg" alt="${skill} 이미지"/>
       </span>
     `);
-  }
+    }
+    $('.about-skill-box .left .bottom span').eq(0).addClass('on');
+    $('.about-skill-box .left .bottom span').click(function () {
+      const skill = $(this).text().trim();
+      $('.about-skill-box .big-img img').attr(
+        'src',
+        `https://site.mypopol.com/ptid02/src/img/skills/${skill.toLowerCase()}.svg`
+      );
+      $('.about-skill-box .left .top .text dt').text(skill);
+      $('.about-skill-box .left .bottom span').removeClass('on');
+      $(this).addClass('on');
+    });
+  };
   $('.about-skill-box .left .bottom span').eq(0).addClass('on');
   // left
   for (let category in skills) {
@@ -27,14 +39,12 @@ $(function () {
     $('.about-skill-box .right div').eq(0).addClass('on');
   }
 
-  $('.about-skill-box .left .bottom span').click(function () {
-    const skill = $(this).text().trim();
-    $('.about-skill-box .big-img img').attr(
-      'src',
-      `https://site.mypopol.com/ptid02/src/img/skills/${skill.toLowerCase()}.svg`
-    );
-    $('.about-skill-box .left .top .text dt').text(skill);
-    $('.about-skill-box .left .bottom span').removeClass('on');
+  $('.about-skill-box .right div').click(function () {
+    $('.about-skill-box .left .bottom').children().remove();
+    $('.about-skill-box .right div').removeClass('on');
     $(this).addClass('on');
+    categorySkillsInit($(this).text());
   });
+
+  categorySkillsInit(Object.entries(skills)[0][0]);
 });
